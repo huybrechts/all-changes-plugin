7648536bf940facce7ca9bab6f6cc68f3afad860
Tom Huybrechts <tom.huybrechts@gmail.com>
2015-11-28 23:17:12 +0100

    local changes

---
+++ src/main/java/org/jenkinsci/plugins/all_changes/AllChangesBuildAction.java
@@ -0,0 +1,11 @@
+package org.jenkinsci.plugins.all_changes;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: awpyv
+ * Date: 24/02/13
+ * Time: 16:19
+ * To change this template use File | Settings | File Templates.
+ */
+public class AllChangesBuildAction {
+}
